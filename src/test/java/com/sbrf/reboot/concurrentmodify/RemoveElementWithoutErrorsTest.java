5a2ed315c51320e74e7f3b94242de4038f785f28
VladDow <vladdowgun@gmail.com>
2022-01-26 15:14:42 +0300

    Реализовано удаление элементов из коллекции

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -1,36 +1,71 @@
 package com.sbrf.reboot.concurrentmodify;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
 
+    List<Integer> list;
+
+    @BeforeEach
+    public void setUp() {
+        list = new ArrayList<>(Arrays.asList(1, 2, 2, 3));
+    }
+
     @Test
     public void successConcurrentModificationException() {
-
-        List<Integer> list = new ArrayList() {{
-            add(1);
-            add(2);
-            add(3);
-        }};
 
         assertThrows(ConcurrentModificationException.class, () -> {
             
             for (Integer integer : list) {
                 list.remove(1);
             }
-            
+
         });
 
     }
 
     @Test
-    public void successRemoveElementWithoutErrors() {
+    public void successRemoveElementWithoutErrorsOne() {
+
+        list.removeIf(integer -> integer == 2);
+        assertEquals(2, list.size());
+
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorsTwo() {
+
+        while (list.remove((Integer) 2));
+        assertEquals(2, list.size());
+
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorsThree() {
+
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() == 2) {
+                iterator.remove();
+            }
+        }
+
+        assertEquals(2, list.size());
+
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorsFour() {
+
+        list = list.stream().filter(integer -> !integer.equals(2)).collect(Collectors.toList());
+        assertEquals(2, list.size());
 
     }
 
